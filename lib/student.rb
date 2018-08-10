af618767caafa81df770ff585f79c3887cc6209a
Nikki A <nikki.apostolakis@gmail.com>
2018-08-10 21:32:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,19 @@ class Student
     new_student.save
   end
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     new_student = self.new(row[1], row[2], row[0])#same as Student.new
   end
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
 end
