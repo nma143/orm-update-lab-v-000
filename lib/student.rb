bf5c32d749d910c980d1b3152f8d8fdb88cfa1df
Nikki A <nikki.apostolakis@gmail.com>
2018-08-10 21:29:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,11 +57,11 @@ class Student
     new_student.save
   end
   def self.new_from_db(row)
-      # create a new Student object given a row from the database
-      new_student = self.new #same as Student.new
-      new_student.id = row[0]
-      new_student.name = row[1]
-      new_student.grade = row[2]
-      new_student
+    # create a new Student object given a row from the database
+    new_student = self.new #same as Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 end
